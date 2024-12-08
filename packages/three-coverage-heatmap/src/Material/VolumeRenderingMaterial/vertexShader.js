689e6cf8373b118c1e5eda37ba6254cec6182c6f
tsengyushiang <tsengyushiang@gmail.com>
2024-12-08 03:09:12 +0000

    fix: volume rendering viewport should cover whole page.

--- packages/three-coverage-heatmap/src/Material/VolumeRenderingMaterial/vertexShader.js
+++ packages/three-coverage-heatmap/src/Material/VolumeRenderingMaterial/vertexShader.js
@@ -1,17 +1,16 @@
 const getVertexShader = () => `
-
 varying vec3 vRayDirection;
 varying vec3 vRayOrigin;
 
 void main() {
-
+  vec4 viewPos = inverse(projectionMatrix) * vec4(position, 1.0);
   vec4 cameraPosition = inverse(viewMatrix) * vec4(0.0, 0.0, 0.0, 1.0);
-  vec4 worldPosition = inverse(viewMatrix) * vec4(position, 1.0);
+  vec4 worldPosition = inverse(viewMatrix) * vec4(viewPos.xy, -1.0, 1.0);
   vec3 rayDirection = normalize(worldPosition.xyz - cameraPosition.xyz);
   vRayDirection = rayDirection;
   vRayOrigin = cameraPosition.xyz;
 
-  gl_Position = projectionMatrix * vec4(position, 1.0);
+  gl_Position = projectionMatrix * vec4(viewPos.xy, -1.0, 1.0);
 }
 `;
 
